99a134a000fc77f79fbb9cca02dfb855c123dd8c
HP 840 <yasin80545859@.com>
2022-05-23 11:56:25 +0600

    Mongodb database Setup Complate

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lfbfx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('Menufecturer Connected')
+  // perform actions on the collection object
+  client.close();
+});
+
 
 //middlware
 app.use(cors());
