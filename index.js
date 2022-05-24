49eb0de9f42c985c2c193035565b6c3cfedde82b
HP 840 <yasin80545859@.com>
2022-05-24 23:25:03 +0600

    get my order

--- index.js
+++ index.js
@@ -31,6 +31,15 @@ async function run(){
             res.send(item);
         })
 
+        app.get('/booking', async (req, res)=>{
+            const email = req.query.email;
+            const query = {email: email};
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
+        
+
         app.post('/booking', async (req, res) =>{
             const booking = req.body;
             const result = await bookingCollection.insertOne(booking);
