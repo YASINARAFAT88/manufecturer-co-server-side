6e3bdcef699d6f3e9a327bc3d5c31b878d8c7008
HP 840 <yasin80545859@.com>
2022-05-24 18:34:51 +0600

    Apply POST Method And recived Orders

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -10,13 +10,39 @@ const app = express();
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lfbfx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('Menufecturer Connected')
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+    try{
+        await client.connect();
+        const itemsCollection = client.db('Menufecturer-Co').collection('items');
+        const bookingCollection = client.db('Menufecturer-Co').collection('booking');
+
+        app.get('/items', async (req, res)=>{
+            const query = {};
+            const cursor = itemsCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        })
+
+        app.get('/items/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const item = await itemsCollection.findOne(query);
+            res.send(item);
+        })
+
+        app.post('/booking', async (req, res) =>{
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
 
 //middlware
 app.use(cors());
