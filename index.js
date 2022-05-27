3485aaadeeef74d5f1f0faeedafd64baf721e6e7
HP 840 <yasin80545859@.com>
2022-05-27 22:28:57 +0600

    implement user remove system

--- index.js
+++ index.js
@@ -84,13 +84,6 @@ async function run(){
             res.send(users)
         })
 
-        // app.get('/admin/:email', async (req, res) => {
-        //     const email = req.params.email;
-        //     const user = await userCollection.findOne({ email: email });
-        //     const isAdmin = user.role === 'admin';
-        //     res.send({ admin: isAdmin })
-        //   })
-
         app.put('/user/admin/:email', verifyJWT, async (req, res) =>{
             const email = req.params.email;
             
@@ -102,10 +95,6 @@ async function run(){
                 }
                 const result = await userCollection.updateOne(filter, updateDoc)
                 res.send(result)
-            
-            // else{
-            //     res.status(403).send({message: 'forbidden'})
-            // }
         })
 
         app.put('/user/:email', async (req, res) =>{
